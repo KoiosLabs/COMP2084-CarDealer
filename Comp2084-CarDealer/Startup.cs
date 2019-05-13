6af31ed1a265c22ef9d154edfc548bfa7ce94757
Justin Hunt <Justin.hunt@utoronto.ca>
2019-05-13 17:01:02 -0400

    Add project files.

---
+++ Comp2084-CarDealer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp2084_CarDealer.Startup))]
+namespace Comp2084_CarDealer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
